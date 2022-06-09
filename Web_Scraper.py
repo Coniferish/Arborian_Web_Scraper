8c0eaf02f205f76b77641f50bd3f686803d0a20a
Coniferish <johnjennings702@gmail.com>
2022-06-09 10:59:15 -0400

    blacken file

--- Web_Scraper.py
+++ Web_Scraper.py
@@ -1,9 +1,9 @@
 import requests
 import pandas as pd
 
-website = 'https://3g8hkyvn3l.execute-api.us-east-1.amazonaws.com/production/product?size=6&page=1'
+website = "https://3g8hkyvn3l.execute-api.us-east-1.amazonaws.com/production/product?size=6&page=1"
 page = requests.get(website)
-product_df = pd.DataFrame(page.json()['products'])
+product_df = pd.DataFrame(page.json()["products"])
 
 # Exercise 1
 def get_product_info(brand):
@@ -11,39 +11,63 @@ def get_product_info(brand):
     temp = []
     brand_df = pd.DataFrame()
 
-    brands = product_df['brand'].values
+    brands = product_df["brand"].values
 
     if brand in brands:
         temp.append(brand)
     else:
         print("That brand isn't carried.")
-            
-    brand_df = pd.concat([brand_df, product_df.loc[product_df['brand'].isin(temp)]], ignore_index=True, sort = False)
-    brand_df[['productId', 'name', 'currentPrice']].to_csv('Exercise1.csv', encoding='utf-8', index = False)
-    return(print("Your brand file is saved."))
+
+    brand_df = pd.concat(
+        [brand_df, product_df.loc[product_df["brand"].isin(temp)]],
+        ignore_index=True,
+        sort=False,
+    )
+    brand_df[["productId", "name", "currentPrice"]].to_csv(
+        "Exercise1.csv", encoding="utf-8", index=False
+    )
+    return print("Your brand file is saved.")
 
 
 # Exercise 2
-id_list = [132602,296804,198765,189958,750518,688028,127853,261549,382587,389471,251184,601484]
+id_list = [
+    132602,
+    296804,
+    198765,
+    189958,
+    750518,
+    688028,
+    127853,
+    261549,
+    382587,
+    389471,
+    251184,
+    601484,
+]
+
 
 def check_inventory(id_list):
     """Check the inventory for a list of products"""
-    inventory_df = pd.DataFrame() 
-    in_stock_ids = product_df['productId'].values  # avoid having to catch a KeyError if item isn't in product_df
+    inventory_df = pd.DataFrame()
+    in_stock_ids = product_df[
+        "productId"
+    ].values  # avoid having to catch a KeyError if item isn't in product_df
 
     for item in id_list:
         if item in in_stock_ids:
-            in_stock_item = product_df.loc[product_df['productId'] == item, ['productId', 'inStock']]
+            in_stock_item = product_df.loc[
+                product_df["productId"] == item, ["productId", "inStock"]
+            ]
             inventory_df = pd.concat([inventory_df, in_stock_item], ignore_index=True)
-        else: 
-            no_item = pd.DataFrame({'productId': [item], 'inStock': [0]})
+        else:
+            no_item = pd.DataFrame({"productId": [item], "inStock": [0]})
             inventory_df = pd.concat([inventory_df, no_item], ignore_index=True)
-    
+
     # inventory_df = pd.concat([inventory_df, huh], ignore_index=True, sort = False)
-    inventory_df.to_csv('Exercise2.csv', encoding='utf-8', index = False)
-    return(print("Your product ID file is saved."))
+    inventory_df.to_csv("Exercise2.csv", encoding="utf-8", index=False)
+    return print("Your product ID file is saved.")
 
 
-if __name__ == '__main__':
-    get_product_info('Foo INC')
+if __name__ == "__main__":
+    get_product_info("Foo INC")
     check_inventory(id_list)
